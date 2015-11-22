f4e4cac5ffabb6e91625fee3b548e2b821e219ae
teststaybaka <teststaybaka@gmail.com>
2015-11-22 16:24:16 -0500

    'testing'

--- server.js
+++ server.js
@@ -108,8 +108,8 @@ twitterStream.on('data', function(tweet) {
   });
 });
 
-var sqs = new AWS.SQS(options={
-  regin: 'us-west-2'
+var sqs = new AWS.SQS(options = {
+  region: 'us-west-2'
 });
 sqs.createQueue({
   QueueName: 'MyQueue'
