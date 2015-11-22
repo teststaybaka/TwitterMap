d2724385170dd8eda8f375efc35d7293f75ba3bb
teststaybaka <teststaybaka@gmail.com>
2015-11-22 18:20:25 -0500

    delete sqs message

--- server.js
+++ server.js
@@ -108,42 +108,53 @@ var server = http.createServer(function (request, response) {
 //   });
 // });
 
-console.log('??????')
-// var sqs = new AWS.SQS(options = {
-//   accessKeyId: process.env.aws_access_key_id,
-//   secretAccessKey: process.env.aws_secret_access_key,
-//   region: 'us-west-2',
-// });
-// sqs.createQueue({
-//   QueueName: 'TwitQueue'
-// }, function(err, data) {
-//   if (err) {
-//     console.log(err, err.stack);
-//     return;
-//   }
-//   console.log(data);
-//   var QueueUrl = data.QueueUrl;
+var sqs = new AWS.SQS(options = {
+  accessKeyId: process.env.aws_access_key_id,
+  secretAccessKey: process.env.aws_secret_access_key,
+  region: 'us-west-2',
+});
+sqs.createQueue({
+  QueueName: 'TwitQueue'
+}, function(err, data) {
+  if (err) {
+    console.log(err, err.stack);
+    return;
+  }
+  console.log(data);
+  var QueueUrl = data.QueueUrl;
 
-//   sqs.sendMessage({
-//     MessageBody: 'Hello world!',
-//     QueueUrl: QueueUrl,
-//   }, function(err, data) {
-//     if (err) {
-//       console.log(err, err.stack);
-//       return;
-//     }
-//     console.log(data);
+  sqs.sendMessage({
+    MessageBody: 'Hello world!',
+    QueueUrl: QueueUrl,
+  }, function(err, data) {
+    if (err) {
+      console.log(err, err.stack);
+      return;
+    }
+    console.log(data);
 
-//     sqs.receiveMessage({
-//       QueueUrl: QueueUrl,
-//       VisibilityTimeout: 0,
-//       WaitTimeSeconds : 10,
-//     }, function(err, data) {
-//       if (err) {
-//         console.log(err, err.stack);
-//         return;
-//       }
-//       console.log(data);
-//     })
-//   });
-// });
+    sqs.receiveMessage({
+      QueueUrl: QueueUrl,
+      VisibilityTimeout: 0,
+      WaitTimeSeconds : 10,
+    }, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        return;
+      }
+      var messages = data.Messages;
+      console.log(messages[0]);
+
+      sqs.deleteMessage({
+        QueueUrl: QueueUrl,
+        ReceiptHandle: messages[0].ReceiptHandle,
+      }, function(err, data) {
+        if (err) {
+          console.log(err, err.stack);
+          return;
+        }
+        console.log(data);
+      });
+    })
+  });
+});
