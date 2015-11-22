962c56906a93ab076017d2f1c254c153e4492689
teststaybaka <teststaybaka@gmail.com>
2015-11-22 16:36:24 -0500

    setting up sqs

--- server.js
+++ server.js
@@ -109,7 +109,9 @@ twitterStream.on('data', function(tweet) {
 });
 
 var sqs = new AWS.SQS(options = {
-  region: 'us-west-2'
+  accessKeyId: process.env.aws_access_key_id,
+  secretAccessKey: process.env.aws_secret_access_key,
+  region: 'us-west-2',
 });
 sqs.createQueue({
   QueueName: 'MyQueue'
