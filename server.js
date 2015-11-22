369d3e6258eeeeb7b5ddd7b1d112f075425687b7
teststaybaka <teststaybaka@gmail.com>
2015-11-22 17:35:16 -0500

    connected

--- server.js
+++ server.js
@@ -24,67 +24,67 @@ var AWS = require('aws-sdk');
 //   response.end();
 // }
 
-// var server = http.createServer(function (request, response) {
-//   // console.log(request.url);
-//   if (request.url === '/') {
-//     response.writeHead(200, {'Content-Type': 'text/html'});
-//     fs.readFile('./map.html', function (err, html) {
-//       if (err) {
-//         throw err;
-//       }
-//       response.write(html);
-//       response.end();
-//     });
-//   } else if (request.url === '/tweets') {
-//     response.writeHead(200, {'Content-Type': 'application/json'});
-//     connection.query('select text, longitude, latitude, lang, created_at, screen_name, image_url from streamdata', function (err, result) {
-//       if (err) {
-//         response.write(JSON.stringify({error: err}));
-//       } else {
-//         response.write(JSON.stringify({tweets: result, histogram: histogram}));
-//       }
-//       response.end();
-//     });
-//   } else if (request.url === '/sample') {
-//     response.writeHead(200, {'Content-Type': 'text/html'});
-//     connection.query('select * from streamdata limit 100', function (err, result) {
-//       if (err) {
-//         response.write('select:'+err);
-//       } else {
-//         response.write('<html><head><meta charset="UTF-8"></head><body><p>select:'+JSON.stringify(result)+'</p></body></html>');
-//       }
-//       response.end();
-//     });
-//   } else if (request.url === '/count') {
-//     response.writeHead(200, {'Content-Type': 'text/html'});
-//     connection.query('select count(*) as total_tweets from streamdata', function (err, result) {
-//       if (err) {
-//         response.write('count:'+err);
-//       } else {
-//         response.write('<html><head><meta charset="UTF-8"></head><body><p>count:'+JSON.stringify(result)+'</p></body></html>');
-//       }
-//       response.end();
-//     });
-//   } else if (static_path.test(request.url)) {
-//     fs.readFile('.'+request.url, function (err, data) {
-//       if (err) {
-//         NotFound(response);
-//       } else {
-//         var parts = request.url.split('/');
-//         var suffix = parts[parts.length - 1].split('.')[1];
-//         var mimeType = mimeTypes[suffix];
+var server = http.createServer(function (request, response) {
+  // console.log(request.url);
+  if (request.url === '/') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./map.html', function (err, html) {
+      if (err) {
+        throw err;
+      }
+      response.write(html);
+      response.end();
+    });
+  } else if (request.url === '/tweets') {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    connection.query('select text, longitude, latitude, lang, created_at, screen_name, image_url from streamdata', function (err, result) {
+      if (err) {
+        response.write(JSON.stringify({error: err}));
+      } else {
+        response.write(JSON.stringify({tweets: result, histogram: histogram}));
+      }
+      response.end();
+    });
+  } else if (request.url === '/sample') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    connection.query('select * from streamdata limit 100', function (err, result) {
+      if (err) {
+        response.write('select:'+err);
+      } else {
+        response.write('<html><head><meta charset="UTF-8"></head><body><p>select:'+JSON.stringify(result)+'</p></body></html>');
+      }
+      response.end();
+    });
+  } else if (request.url === '/count') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    connection.query('select count(*) as total_tweets from streamdata', function (err, result) {
+      if (err) {
+        response.write('count:'+err);
+      } else {
+        response.write('<html><head><meta charset="UTF-8"></head><body><p>count:'+JSON.stringify(result)+'</p></body></html>');
+      }
+      response.end();
+    });
+  } else if (static_path.test(request.url)) {
+    fs.readFile('.'+request.url, function (err, data) {
+      if (err) {
+        NotFound(response);
+      } else {
+        var parts = request.url.split('/');
+        var suffix = parts[parts.length - 1].split('.')[1];
+        var mimeType = mimeTypes[suffix];
 
-//         response.writeHead(200, {'Content-Type': mimeType});
-//         response.write(data);
-//         response.end();
-//       }
-//     });
-//   } else {
-//     NotFound(response);
-//   }
-// }).listen(process.env.PORT || 80, function() {
-//   console.log(server.address());
-// });
+        response.writeHead(200, {'Content-Type': mimeType});
+        response.write(data);
+        response.end();
+      }
+    });
+  } else {
+    NotFound(response);
+  }
+}).listen(process.env.PORT || 80, function() {
+  console.log(server.address());
+});
 
 // var wss = new WebSocketServer({server: server});
 // wss.on('connection', function (ws) {
@@ -108,9 +108,6 @@ var AWS = require('aws-sdk');
 //   });
 // });
 
-console.log(process.env.database_endpoint)
-console.log(process.env.aws_access_key_id)
-console.log(process.env.aws_secret_access_key)
 var sqs = new AWS.SQS(options = {
   accessKeyId: process.env.aws_access_key_id,
   secretAccessKey: process.env.aws_secret_access_key,
