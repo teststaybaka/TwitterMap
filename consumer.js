aa002acdf8fddeeb6c26fbd31e6b8376f325224a
teststaybaka <teststaybaka@gmail.com>
2015-11-22 23:47:25 -0500

    longpolling

--- consumer.js
+++ consumer.js
@@ -1,6 +1,5 @@
+var events = require('events');
 var AWS = require('aws-sdk');
-
-var longPolling = function
 
 var sqs = new AWS.SQS(options = {
     accessKeyId: process.env.aws_access_key_id,
@@ -17,30 +16,42 @@ sqs.createQueue({
     }
     var QueueUrl = data.QueueUrl;
 
-    sqs.receiveMessage({
-        QueueUrl: QueueUrl,
-        VisibilityTimeout: 0,
-        WaitTimeSeconds : 10,
-    }, function(err, data) {
-        if (err) {
-            console.log(err, err.stack);
-            return;
-        }
-        if (!data.Messages) return;
-        
-        var messages = data.Messages;
-        console.log('SQS received.');
-        console.log(messages[0]);
-        sqs.deleteMessage({
+    function LongPolling() {}
+    LongPolling.prototype = new events.EventEmitter;
+    LongPolling.prototype.receive = function() {
+        var self = this;
+        sqs.receiveMessage({
             QueueUrl: QueueUrl,
-            ReceiptHandle: messages[0].ReceiptHandle,
+            VisibilityTimeout: 0,
+            WaitTimeSeconds : 10,
         }, function(err, data) {
+            self.emit('done');
             if (err) {
                 console.log(err, err.stack);
                 return;
             }
-            console.log('SQS deleted.');
-            console.log(data);
+            if (!data.Messages) return;
+            
+            var messages = data.Messages;
+            console.log('SQS received.');
+            console.log(messages[0]);
+            sqs.deleteMessage({
+                QueueUrl: QueueUrl,
+                ReceiptHandle: messages[0].ReceiptHandle,
+            }, function(err, data) {
+                if (err) {
+                    console.log(err, err.stack);
+                    return;
+                }
+                console.log('SQS deleted.');
+                console.log(data);
+            });
         });
+    }
+
+    var longPolling = new LongPolling();
+    longPolling.on('done', function() {
+        longPolling.receive();
     });
+    longPolling.receive();
 });
