2616355bae7f46405c9cb212d7eca9008a057c53
teststaybaka <teststaybaka@gmail.com>
2015-11-23 23:42:08 -0500

    debug

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var server = http.createServer(function (request, response) {
       if (err) {
         response.write(JSON.stringify({error: err}));
       } else {
-        response.write(JSON.stringify({tweets: result, histogram: histogram}));
+        response.write(JSON.stringify(result));
       }
       response.end();
     });
