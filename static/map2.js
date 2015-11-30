ea0d6e8cb5ffdc845a8459359525514981a31ad6
teststaybaka <teststaybaka@gmail.com>
2015-11-29 19:22:29 -0500

    icons

--- static/map2.js
+++ static/map2.js
@@ -47,7 +47,7 @@ google.maps.event.addDomListener(window, 'load', function() {
                     width: 50
                 }
             ];
-            var markerCluster = new MarkerClusterer(map, markers, {ignoreHidden: true, gridSize: 50, maxZoom: 15});
+            var markerCluster = new MarkerClusterer(map, markers, {ignoreHidden: true, gridSize: 50, maxZoom: 15, styles: clusterStyles});
             markerCluster.setCalculator(function(markers, numStyles) {
                 // var index = 0;
                 // var count = markers.length;
